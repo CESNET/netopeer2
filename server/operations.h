5e6285169298594772ed35d3fa23aa5c17a8a17e
Michal Vasko <mvasko@cesnet.cz>
2016-05-19 13:14:13 +0200

    server VERSION bump to version 0.1.31

--- server/operations.h
+++ server/operations.h
@@ -64,7 +64,6 @@ struct nc_server_reply *op_lock(struct lyd_node *rpc, struct nc_session *ncs);
 struct nc_server_reply *op_unlock(struct lyd_node *rpc, struct nc_session *ncs);
 struct nc_server_reply *op_editconfig(struct lyd_node *rpc, struct nc_session *ncs);
 struct nc_server_reply *op_copyconfig(struct lyd_node *rpc, struct nc_session *ncs);
-
 struct nc_server_reply *op_generic(struct lyd_node *rpc, struct nc_session *ncs);
 
 #endif /* NP2SRV_OPERATIONS_H_ */
