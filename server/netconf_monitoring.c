163951040605091f1318e2c3d7adfb5e6dc77b04
Michal Vasko <mvasko@cesnet.cz>
2016-05-25 11:21:40 +0200

    server FEATURE netconf monitoring fully implemented

--- server/netconf_monitoring.c
+++ server/netconf_monitoring.c
@@ -23,6 +23,9 @@
 #include "netconf_monitoring.h"
 #include "common.h"
 #include "log.h"
+#include "operations.h"
+
+#define NCM_TIMEZONE "CET"
 
 struct np_session_stats {
     uint32_t in_rpcs;
@@ -182,14 +185,11 @@ ncm_bad_hello(void)
 struct lyd_node *
 ncm_get_data(void)
 {
-    struct lyd_node *yanglib, *root = NULL, *cont;
+    struct lyd_node *root = NULL, *cont, *list, *cont2;
+    const struct lys_module *mod;
     const char **cpblts;
-    int i;
-
-    yanglib = ly_ctx_info(np2srv.ly_ctx);
-    if (!yanglib) {
-        goto error;
-    }
+    char buf[26];
+    uint32_t i;
 
     root = lyd_new_path(NULL, np2srv.ly_ctx, "/ietf-netconf-monitoring:netconf-state", NULL, 0);
     if (!root) {
@@ -211,22 +211,133 @@ ncm_get_data(void)
     free(cpblts);
 
     /* datastores */
-    /* TODO */
+    pthread_rwlock_rdlock(&dslock_rwl);
+
+    cont = lyd_new(root, NULL, "datastores");
+
+    list = lyd_new(cont, NULL, "datastore");
+    lyd_new_leaf(list, NULL, "name", "running");
+    if (dslock.running) {
+        cont2 = lyd_new(list, NULL, "global-lock");
+        sprintf(buf, "%u", nc_session_get_id(dslock.running));
+        lyd_new_leaf(cont2, NULL, "locked-by-session", buf);
+        nc_time2datetime(dslock.running_time, NCM_TIMEZONE, buf);
+        lyd_new_leaf(cont2, NULL, "locked-time", buf);
+    }
+
+    list = lyd_new(cont, NULL, "datastore");
+    lyd_new_leaf(list, NULL, "name", "startup");
+    if (dslock.startup) {
+        cont2 = lyd_new(list, NULL, "global-lock");
+        sprintf(buf, "%u", nc_session_get_id(dslock.startup));
+        lyd_new_leaf(cont2, NULL, "locked-by-session", buf);
+        nc_time2datetime(dslock.startup_time, NCM_TIMEZONE, buf);
+        lyd_new_leaf(cont2, NULL, "locked-time", buf);
+    }
+
+    list = lyd_new(cont, NULL, "datastore");
+    lyd_new_leaf(list, NULL, "name", "candidate");
+    if (dslock.candidate) {
+        cont2 = lyd_new(list, NULL, "global-lock");
+        sprintf(buf, "%u", nc_session_get_id(dslock.candidate));
+        lyd_new_leaf(cont2, NULL, "locked-by-session", buf);
+        nc_time2datetime(dslock.candidate_time, NCM_TIMEZONE, buf);
+        lyd_new_leaf(cont2, NULL, "locked-time", buf);
+    }
+
+    pthread_rwlock_unlock(&dslock_rwl);
 
     /* schemas */
-    /* TODO */
+    cont = lyd_new(root, NULL, "schemas");
+
+    i = 0;
+    while ((mod = ly_ctx_get_module_iter(np2srv.ly_ctx, &i))) {
+        list = lyd_new(cont, NULL, "schema");
+        lyd_new_leaf(list, NULL, "identifier", mod->name);
+        lyd_new_leaf(list, NULL, "version", (mod->rev ? mod->rev[0].date : NULL));
+        lyd_new_leaf(list, NULL, "format", "yang");
+        lyd_new_leaf(list, NULL, "namespace", lys_main_module(mod)->ns);
+        lyd_new_leaf(list, NULL, "location", "NETCONF");
+
+        list = lyd_new(cont, NULL, "schema");
+        lyd_new_leaf(list, NULL, "identifier", mod->name);
+        lyd_new_leaf(list, NULL, "version", (mod->rev ? mod->rev[0].date : NULL));
+        lyd_new_leaf(list, NULL, "format", "yin");
+        lyd_new_leaf(list, NULL, "namespace", lys_main_module(mod)->ns);
+        lyd_new_leaf(list, NULL, "location", "NETCONF");
+    }
 
     /* sessions */
-    /* TODO */
+    pthread_mutex_lock(&stats.lock);
+
+    if (stats.session_count) {
+        cont = lyd_new(root, NULL, "sessions");
+
+        for (i = 0; i < stats.session_count; ++i) {
+            list = lyd_new(cont, NULL, "session");
+
+            sprintf(buf, "%u", nc_session_get_id(stats.sessions[i]));
+            lyd_new_leaf(list, NULL, "session-id", buf);
+            switch (nc_session_get_ti(stats.sessions[i])) {
+#ifdef NC_ENABLED_SSH
+            case NC_TI_LIBSSH:
+                lyd_new_leaf(list, NULL, "transport", "netconf-ssh");
+                break;
+#endif
+#ifdef NC_ENABLED_TLS
+            case NC_TI_OPENSSL:
+                lyd_new_leaf(list, NULL, "transport", "netconf-tls");
+                break;
+#endif
+            default: /* NC_TI_FD, NC_TI_NONE */
+                lyd_new_leaf(list, NULL, "transport", "transport");
+                break;
+            }
+            lyd_new_leaf(list, NULL, "username", nc_session_get_username(stats.sessions[i]));
+            lyd_new_leaf(list, NULL, "source-host", nc_session_get_host(stats.sessions[i]));
+            nc_time2datetime(nc_session_get_start_time(stats.sessions[i]), NCM_TIMEZONE, buf);
+            lyd_new_leaf(list, NULL, "login-time", buf);
+
+            sprintf(buf, "%u", stats.session_stats[i].in_rpcs);
+            lyd_new_leaf(list, NULL, "in-rpcs", buf);
+            sprintf(buf, "%u", stats.session_stats[i].in_bad_rpcs);
+            lyd_new_leaf(list, NULL, "in-bad-rpcs", buf);
+            sprintf(buf, "%u", stats.session_stats[i].out_rpc_errors);
+            lyd_new_leaf(list, NULL, "out-rpc-errors", buf);
+            sprintf(buf, "%u", stats.session_stats[i].out_notifications);
+            lyd_new_leaf(list, NULL, "out-notifications", buf);
+        }
+    }
 
     /* statistics */
-    /* TODO */
+    cont = lyd_new(root, NULL, "statistics");
 
-    lyd_free(yanglib);
+    nc_time2datetime(stats.netconf_start_time, NCM_TIMEZONE, buf);
+    lyd_new_leaf(cont, NULL, "netconf-start-time", buf);
+    sprintf(buf, "%u", stats.in_bad_hellos);
+    lyd_new_leaf(cont, NULL, "in-bad-hellos", buf);
+    sprintf(buf, "%u", stats.in_sessions);
+    lyd_new_leaf(cont, NULL, "in-sessions", buf);
+    sprintf(buf, "%u", stats.dropped_sessions);
+    lyd_new_leaf(cont, NULL, "dropped-sessions", buf);
+    sprintf(buf, "%u", stats.global_stats.in_rpcs);
+    lyd_new_leaf(cont, NULL, "in-rpcs", buf);
+    sprintf(buf, "%u", stats.global_stats.in_bad_rpcs);
+    lyd_new_leaf(cont, NULL, "in-bad-rpcs", buf);
+    sprintf(buf, "%u", stats.global_stats.out_rpc_errors);
+    lyd_new_leaf(cont, NULL, "out-rpc-errors", buf);
+    sprintf(buf, "%u", stats.global_stats.out_notifications);
+    lyd_new_leaf(cont, NULL, "out-notifications", buf);
+
+    pthread_mutex_unlock(&stats.lock);
+
+    if (lyd_validate(&root, LYD_OPT_GET)) {
+        goto error;
+    }
+
     return root;
 
 error:
-    lyd_free(yanglib);
     lyd_free(root);
     return NULL;
 }
