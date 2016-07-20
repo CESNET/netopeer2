a536778425bd2ec0bbd4c37ffbe04882fdac9dfe
Michal Vasko <mvasko@cesnet.cz>
2016-07-20 15:04:17 +0200

    travis TEST use the cache normally again

--- .travis-install-libs.sh
+++ .travis-install-libs.sh
@@ -5,7 +5,7 @@ sudo apt-get install -y libavl-dev libev-dev
 sudo apt-get install -y zlib1g-dev libssl-dev
 sudo apt-get install -y valgrind
 
-if [  -d "cmocka-1.0.1" ]; then
+if [ ! -d "cmocka-1.0.1" ]; then
     echo "Building cmocka from source."
     wget https://cmocka.org/files/1.0/cmocka-1.0.1.tar.xz
     tar -xJvf cmocka-1.0.1.tar.xz
@@ -25,7 +25,7 @@ cmake -DCMAKE_INSTALL_PREFIX:PATH=/usr -DCMAKE_BUILD_TYPE=Release ..
 make -j2 && sudo make install
 cd ../..
 
-if [  -d "libssh-0.7.3" ]; then
+if [ ! -d "libssh-0.7.3" ]; then
     echo "Building libssh from source."
     wget https://git.libssh.org/projects/libssh.git/snapshot/libssh-0.7.3.tar.bz2
     tar -xjf libssh-0.7.3.tar.bz2
@@ -45,7 +45,7 @@ cmake -DCMAKE_INSTALL_PREFIX:PATH=/usr -DCMAKE_BUILD_TYPE=Release ..
 make -j2 && sudo make install
 cd ../..
 
-if [  -d "protobuf" ]; then
+if [ ! -d "protobuf" ]; then
     echo "Building protobuf from source."
     git clone https://github.com/google/protobuf.git
     cd protobuf
@@ -58,7 +58,7 @@ else
     cd ..
 fi
 
-if [  -d "protobuf-c" ]; then
+if [ ! -d "protobuf-c" ]; then
     echo "Building protobuf-c from source."
     git clone https://github.com/protobuf-c/protobuf-c.git
     cd protobuf-c
