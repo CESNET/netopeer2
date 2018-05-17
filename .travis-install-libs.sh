3dd94fe7d96fde848e5a2d0925337b26b2998d6f
Radek Krejci <rkrejci@cesnet.cz>
2018-05-17 09:01:15 +0200

    travis CHANGE use cmocka 1.1.1 since 1.0.1 has issues with clang

--- .travis-install-libs.sh
+++ .travis-install-libs.sh
@@ -10,16 +10,16 @@ sudo apt-get update -qq
 sudo apt-get install -y zlib1g-dev libssl-dev
 sudo apt-get install -y --force-yes libavl-dev libev-dev coreutils acl valgrind
 
-if [ ! -d "cmocka-1.0.1/build" ]; then
+if [ ! -d "cmocka-1.1.1/build" ]; then
     echo "Building cmocka from source."
-    wget https://cmocka.org/files/1.0/cmocka-1.0.1.tar.xz
-    tar -xJvf cmocka-1.0.1.tar.xz
-    cd cmocka-1.0.1 && mkdir build && cd build
+    wget https://cmocka.org/files/1.1/cmocka-1.1.1.tar.xz
+    tar -xJvf cmocka-1.1.1.tar.xz
+    cd cmocka-1.1.1 && mkdir build && cd build
     cmake -DCMAKE_INSTALL_PREFIX:PATH=/usr .. && make -j2 && sudo make install
     cd ../..
 else
     echo "Using cmocka from cache."
-    cd cmocka-1.0.1/build
+    cd cmocka-1.1.1/build
     sudo make install
     cd ../..
 fi
