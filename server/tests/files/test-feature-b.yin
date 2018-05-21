CESNET/netopeer2

commit 6cb1612f612003648b5b9bea90ff9dec8fb6b645

server OPTIMIZE formatting and optimizations
Refs #248

server/main.c
@@ -755,6 +755,7 @@ np2srv_init_schemas(void)
     const struct lys_module *mod;
     sr_schema_t *schemas = NULL;
     size_t schema_count, i, j;
+    int ly_lo;
 
     size_t module_feature_count = 0;
     struct mod_feat {
@@ -813,10 +814,18 @@ np2srv_init_schemas(void)
     }
     ly_ctx_set_module_imp_clb(np2srv.ly_ctx, np2srv_ly_import_clb, NULL);
 
+    /* determine the count of module/features present */
+    module_feature_count = 0;
+    for (i = 0; i < schema_count; i++) {
+        module_feature_count += schemas[i].enabled_feature_cnt;
+    }
+    /* prepare data storage to keep module/feature enable status */
+    mod_feat_array = calloc(module_feature_count, sizeof *mod_feat_array);
+    mod_feat_array_index = 0;
+
     /* 1) use modules from sysrepo */
     for (i = 0; i < schema_count; i++) {
         data = NULL;
-        mod = NULL;
         VRB("Loading schema \"%s%s%s\" from sysrepo.", schemas[i].module_name, schemas[i].revision.revision ? "@" : "",
             schemas[i].revision.revision ? schemas[i].revision.revision : "");
         if ((mod = ly_ctx_get_module(np2srv.ly_ctx, schemas[i].module_name, schemas[i].revision.revision, 0))) {
@@ -833,30 +842,25 @@ np2srv_init_schemas(void)
             mod = lys_parse_mem(np2srv.ly_ctx, data, LYS_IN_YIN);
             free(data);
         }
-    }
 
-    /* determine the count of module/features present */
-    module_feature_count = 0;
-    for (i=0; i < schema_count; i++) {
-        module_feature_count += schemas[i].enabled_feature_cnt;
-    }
-    /* prepare data storage to keep module/feature enable status */
-    mod_feat_array = calloc(module_feature_count, sizeof (struct mod_feat));
-    mod_feat_array_index = 0;
+        if (mod) {
+            /* set RPC and Notifications callbacks */
+            np2srv_module_assign_clbs(mod);
 
-    /* loop back through the modules now that they should be loaded and enable features */
-    for (i = 0; i < schema_count; i++) {
-        if ((mod = ly_ctx_get_module(np2srv.ly_ctx, schemas[i].module_name, schemas[i].revision.revision, 0))) {
-            /* set features according to sysrepo */
+            /* set features according to sysrepo (silent errors, there can be some other dependent features not enabled yet) */
+            ly_lo = ly_log_options(0);
             for (j = 0; j < schemas[i].enabled_feature_cnt; ++j) {
-                /* if lys_features_enable fail, record the mod_feat for retry otherwise store a null */
-                if (lys_features_enable(mod, schemas[i].enabled_features[j]) == EXIT_FAILURE) {
+                /* if lys_features_enable fail (silently), record the mod_feat for retry otherwise store NULL */
+
+                if (lys_features_enable(mod, schemas[i].enabled_features[j])) {
                     /* store away info related to feature that did not enable on first pass */
                     mod_feat_array[mod_feat_array_index].mod = mod;
                     mod_feat_array[mod_feat_array_index].enabled_feature = schemas[i].enabled_features[j];
                 }
                 mod_feat_array_index++;
             }
+            /* restore libyang logging */
+            ly_log_options(ly_lo);
         } else {
             WRN("Getting %s%s%s schema from sysrepo failed, data from this module won't be available.",
                 schemas[i].module_name, schemas[i].revision.revision ? "@" : "",
@@ -867,15 +871,14 @@ np2srv_init_schemas(void)
     /* we may be able to get features enabled now that others are enabled so we will
        run through our stored module_features and retry the ones on our list
        we will continue until a pass through the array doesn't indicate progress */
-    making_progress = true;
-    while(making_progress) {
+    ly_lo = ly_log_options(0);
+    making_progress = false;
+    while (making_progress) {
         making_progress = false;
         for (i = 0; i < module_feature_count; i++) {
-            if (mod_feat_array[i].mod != NULL) {
-                if ((lys_features_enable(mod_feat_array[i].mod, mod_feat_array[i].enabled_feature)) == EXIT_SUCCESS) {
+            if (mod_feat_array[i].mod) {
+                if (!lys_features_enable(mod_feat_array[i].mod, mod_feat_array[i].enabled_feature)) {
                     /* feature enabled, remove from list and set making_progress TRUE */
-                    VRB("Retry feature enable successful for \"%s\" in \"%s\".",
-                        mod_feat_array[i].enabled_feature, mod_feat_array[i].mod->name);
                     making_progress = true;
                     mod_feat_array[i].mod = NULL;
                     mod_feat_array[i].enabled_feature = NULL;
@@ -883,25 +886,17 @@ np2srv_init_schemas(void)
             }
         }
     }
+    ly_log_options(ly_lo);
 
-    /* now pass through the mod_feat_array and report ERR on any remaining entries
+    /* now pass through the mod_feat_array and report errors on any remaining entries
        as they are not enabled */
     for (i = 0; i < module_feature_count; i++) {
-        if (mod_feat_array[i].mod != NULL) {
-            /* this module feature never enabled */
-            ERR("Retry feature enable failed for \"%s\" in \"%s\".",
-                mod_feat_array[i].enabled_feature, mod_feat_array[i].mod->name);
+        if (mod_feat_array[i].mod) {
+            lys_features_enable(mod_feat_array[i].mod, mod_feat_array[i].enabled_feature);
         }
     }
 
     free(mod_feat_array);
-
-    for (i = 0; i < schema_count; i++) {
-        if ((mod = ly_ctx_get_module(np2srv.ly_ctx, schemas[i].module_name, schemas[i].revision.revision, 0))) {
-            /* setRPC and Notifications callbacks */
-            np2srv_module_assign_clbs(mod);
-        }
-    }
 
     ly_ctx_set_module_imp_clb(np2srv.ly_ctx, np2srv_ly_import_clb, NULL);
     sr_free_schemas(schemas, schema_count);

commit 62c408e4fb03082e17a160c7dd9b594b2e2c91b9

Fix longer feature dep chains
This is a follow-up to pull request #248, which fixed a problem enabling a
feature that depended on a feature in a subsequent schema that had not yet
been enabled.

The follow-up optimizations in commit 6cb1612 inadvertently changed the
desired behavior, in that a feature dependency chain more than two features
deep will no longer be enabled properly. The problem is that the
`making_progress` loop in main.c line 876 is never entered.

I've included a change to the unit test from #248 that demonstrates a
feature dependency chain three features deep, instead of the original test
which was only two features deep.

server/main.c
@@ -872,7 +872,7 @@ np2srv_init_schemas(void)
        run through our stored module_features and retry the ones on our list
        we will continue until a pass through the array doesn't indicate progress */
     ly_lo = ly_log_options(0);
-    making_progress = false;
+    making_progress = true;
     while (making_progress) {
         making_progress = false;
         for (i = 0; i < module_feature_count; i++) {

server/tests/config.h.in
@@ -124,10 +124,12 @@ __wrap_sr_get_schema(sr_session_ctx_t *session, const char *module_name, const c
         fd = open(TESTS_DIR "/files/nc-notifications.yin", O_RDONLY);
     } else if (!strcmp(module_name, "test-notif")) {
         fd = open(TESTS_DIR "/files/test-notif.yin", O_RDONLY);
-    } else if (!strcmp(module_name, "test-feature")) {
-        fd = open(TESTS_DIR "/files/test-feature.yin", O_RDONLY);
-    } else if (!strcmp(module_name, "test-feature-consumer")) {
-        fd = open(TESTS_DIR "/files/test-feature-consumer.yin", O_RDONLY);
+    } else if (!strcmp(module_name, "test-feature-a")) {
+        fd = open(TESTS_DIR "/files/test-feature-a.yin", O_RDONLY);
+    } else if (!strcmp(module_name, "test-feature-b")) {
+        fd = open(TESTS_DIR "/files/test-feature-b.yin", O_RDONLY);
+    } else if (!strcmp(module_name, "test-feature-c")) {
+        fd = open(TESTS_DIR "/files/test-feature-c.yin", O_RDONLY);
     } else {
         return SR_ERR_NOT_FOUND;
     }

server/tests/files/test-feature-a.yin
@@ -1,9 +1,9 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<module name="test-feature"
+<module name="test-feature-a"
         xmlns="urn:ietf:params:xml:ns:yang:yin:1"
-        xmlns:tf="urn:ietf:params:xml:ns:yang:test-feature">
-  <namespace uri="urn:ietf:params:xml:ns:yang:test-feature"/>
-  <prefix value="tf"/>
+        xmlns:tfa="urn:ietf:params:xml:ns:yang:test-feature-a">
+  <namespace uri="urn:ietf:params:xml:ns:yang:test-feature-a"/>
+  <prefix value="tfa"/>
   <revision date="2018-05-18">
     <description>
       <text>Initial revision.</text>
@@ -12,9 +12,9 @@
       <text>None.</text>
     </reference>
   </revision>
-  <feature name="controlling-feature">
+  <feature name="test-feature-a">
     <description>
-      <text>A controlling feature</text>
+      <text>Test feature A</text>
     </description>
   </feature>
 </module>

server/tests/files/test-feature-b.yin
@@ -1,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<module name="test-feature-b"
+        xmlns="urn:ietf:params:xml:ns:yang:yin:1"
+        xmlns:tfb="urn:ietf:params:xml:ns:yang:test-feature-b"
+        xmlns:tfa="urn:ietf:params:xml:ns:yang:test-feature-a">
+  <namespace uri="urn:ietf:params:xml:ns:yang:test-feature-b"/>
+  <prefix value="tfb"/>
+  <import module="test-feature-a">
+    <prefix value="tfa"/>
+  </import>
+  <revision date="2018-05-18">
+    <description>
+      <text>Initial revision.</text>
+    </description>
+    <reference>
+      <text>None.</text>
+    </reference>
+  </revision>
+  <feature name="test-feature-b">
+    <if-feature name="tfa:test-feature-a"/>
+  </feature>
+</module>

server/tests/files/test-feature-c.yin
@@ -1,12 +1,12 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<module name="test-feature-consumer"
+<module name="test-feature-c"
         xmlns="urn:ietf:params:xml:ns:yang:yin:1"
-        xmlns:tfc="urn:ietf:params:xml:ns:yang:test-feature-consumer"
-        xmlns:tf="urn:ietf:params:xml:ns:yang:test-feature">
-  <namespace uri="urn:ietf:params:xml:ns:yang:test-feature-consumer"/>
+        xmlns:tfc="urn:ietf:params:xml:ns:yang:test-feature-c"
+        xmlns:tfb="urn:ietf:params:xml:ns:yang:test-feature-b">
+  <namespace uri="urn:ietf:params:xml:ns:yang:test-feature-c"/>
   <prefix value="tfc"/>
-  <import module="test-feature">
-    <prefix value="tf"/>
+  <import module="test-feature-b">
+    <prefix value="tfb"/>
   </import>
   <revision date="2018-05-18">
     <description>
@@ -16,11 +16,11 @@
       <text>None.</text>
     </reference>
   </revision>
-  <feature name="dependent-feature">
-    <if-feature name="tf:controlling-feature"/>
+  <feature name="test-feature-c">
+    <if-feature name="tfb:test-feature-b"/>
   </feature>
   <container name="test-container">
-    <if-feature name="dependent-feature"/>
+    <if-feature name="test-feature-c"/>
     <leaf name="test-leaf">
       <type name="string"/>
     </leaf>

server/tests/test_edit_get_config.c
@@ -72,9 +72,9 @@ __wrap_sr_list_schemas(sr_session_ctx_t *session, sr_schema_t **schemas, size_t 
 {
     (void)session;
 
-    *schema_cnt = 6;
+    *schema_cnt = 7;
 
-    *schemas = calloc(6, sizeof **schemas);
+    *schemas = calloc(*schema_cnt, sizeof **schemas);
 
     (*schemas)[0].module_name = strdup("ietf-netconf-server");
     (*schemas)[0].installed = 1;
@@ -107,25 +107,35 @@ __wrap_sr_list_schemas(sr_session_ctx_t *session, sr_schema_t **schemas, size_t 
     (*schemas)[3].revision.file_path_yin = strdup(TESTS_DIR"/files/iana-if-type.yin");
     (*schemas)[3].installed = 1;
 
-    (*schemas)[4].module_name = strdup("test-feature-consumer");
-    (*schemas)[4].ns = strdup("urn:ietf:params:xml:ns:yang:test-feature-consumer");
+    (*schemas)[4].module_name = strdup("test-feature-c");
+    (*schemas)[4].ns = strdup("urn:ietf:params:xml:ns:yang:test-feature-c");
     (*schemas)[4].prefix = strdup("tfc");
     (*schemas)[4].revision.revision = strdup("2018-05-18");
-    (*schemas)[4].revision.file_path_yin = strdup(TESTS_DIR"/files/test-feature-consumer.yin");
+    (*schemas)[4].revision.file_path_yin = strdup(TESTS_DIR"/files/test-feature-c.yin");
     (*schemas)[4].enabled_features = malloc(sizeof(char *));
-    (*schemas)[4].enabled_features[0] = strdup("dependent-feature");
+    (*schemas)[4].enabled_features[0] = strdup("test-feature-c");
     (*schemas)[4].enabled_feature_cnt = 1;
     (*schemas)[4].installed = 1;
 
-    (*schemas)[5].module_name = strdup("test-feature");
-    (*schemas)[5].ns = strdup("urn:ietf:params:xml:ns:yang:test-feature");
-    (*schemas)[5].prefix = strdup("tf");
+    (*schemas)[5].module_name = strdup("test-feature-b");
+    (*schemas)[5].ns = strdup("urn:ietf:params:xml:ns:yang:test-feature-b");
+    (*schemas)[5].prefix = strdup("tfb");
     (*schemas)[5].revision.revision = strdup("2018-05-18");
-    (*schemas)[5].revision.file_path_yin = strdup(TESTS_DIR"/files/test-feature.yin");
+    (*schemas)[5].revision.file_path_yin = strdup(TESTS_DIR"/files/test-feature-b.yin");
     (*schemas)[5].enabled_features = malloc(sizeof(char *));
-    (*schemas)[5].enabled_features[0] = strdup("controlling-feature");
+    (*schemas)[5].enabled_features[0] = strdup("test-feature-b");
     (*schemas)[5].enabled_feature_cnt = 1;
     (*schemas)[5].installed = 1;
+
+    (*schemas)[6].module_name = strdup("test-feature-a");
+    (*schemas)[6].ns = strdup("urn:ietf:params:xml:ns:yang:test-feature-a");
+    (*schemas)[6].prefix = strdup("tfa");
+    (*schemas)[6].revision.revision = strdup("2018-05-18");
+    (*schemas)[6].revision.file_path_yin = strdup(TESTS_DIR"/files/test-feature-a.yin");
+    (*schemas)[6].enabled_features = malloc(sizeof(char *));
+    (*schemas)[6].enabled_features[0] = strdup("test-feature-a");
+    (*schemas)[6].enabled_feature_cnt = 1;
+    (*schemas)[6].installed = 1;
 
     return SR_ERR_OK;
 }
@@ -228,7 +238,7 @@ __wrap_sr_get_item_next(sr_session_ctx_t *session, sr_val_iter_t *iter, sr_val_t
     char *path;
     (void)session;
 
-    if (!strcmp(xpath, "/ietf-interfaces:*//.") || !strcmp(xpath, "/test-feature-consumer:*//.")) {
+    if (!strcmp(xpath, "/ietf-interfaces:*//.") || !strcmp(xpath, "/test-feature-c:*//.")) {
         if (!ietf_if_set) {
             ietf_if_set = lyd_find_path(data, xpath);
         }
@@ -1435,7 +1445,7 @@ test_edit_merge2(void **state)
     "<link-up-down-trap-enable>disabled</link-up-down-trap-enable>"
   "</interface>"
 "</interfaces>"
-"<test-container xmlns=\"urn:ietf:params:xml:ns:yang:test-feature-consumer\">"
+"<test-container xmlns=\"urn:ietf:params:xml:ns:yang:test-feature-c\">"
   "<test-leaf>green</test-leaf>"
 "</test-container>"
         "</data>"
@@ -1447,7 +1457,7 @@ test_edit_merge2(void **state)
                 "<running/>"
             "</target>"
             "<config>"
-"<test-container xmlns=\"urn:ietf:params:xml:ns:yang:test-feature-consumer\">"
+"<test-container xmlns=\"urn:ietf:params:xml:ns:yang:test-feature-c\">"
   "<test-leaf>green</test-leaf>"
 "</test-container>"
             "</config>"