CESNET/netopeer2

commit 3ccbdb280917d254ef40b42dc14387a46489e877

server BUGFIX keep previous options when changing config opt
Fixes sysrepo/sysrepo#1163

server/op_get_config.c
@@ -123,18 +123,19 @@ op_get(struct lyd_node *rpc, struct nc_session *ncs)
 
         ly_set_free(nodeset);
     }
-    if (ds != sessions->ds || (sessions->opts & SR_SESS_CONFIG_ONLY) != config_only) {
+    if (ds != sessions->ds) {
         /* update sysrepo session datastore */
         if (np2srv_sr_session_switch_ds(sessions->srs, ds, &ereply)) {
            goto error;
         }
         sessions->ds = ds;
-
+    }
+    if ((sessions->opts & SR_SESS_CONFIG_ONLY) != config_only) {
         /* update sysrepo session config */
-        if (np2srv_sr_session_set_options(sessions->srs, config_only, &ereply)) {
+        if (np2srv_sr_session_set_options(sessions->srs, sessions->opts ^ config_only, &ereply)) {
             goto error;
         }
-        sessions->opts = config_only;
+        sessions->opts ^= config_only;
     }
 
     /* create filters */

commit 14c58b0c3881eec8a899d5db33c0d5b4cffce3ed

server BUGFIX fix session option change using xor
The variable config_only can be either 0 or SR_SESS_CONFIG_ONLY.
XORing with 0 is useless, so I suppose the intention was to toggle
the value of the option, which is instead to XOR with SR_SESS_CONFIG_ONLY.

Fixes: 3ccbdb280917 ("server BUGFIX keep previous options when changing config opt")

server/op_get_config.c
@@ -132,10 +132,10 @@ op_get(struct lyd_node *rpc, struct nc_session *ncs)
     }
     if ((sessions->opts & SR_SESS_CONFIG_ONLY) != config_only) {
         /* update sysrepo session config */
-        if (np2srv_sr_session_set_options(sessions->srs, sessions->opts ^ config_only, &ereply)) {
+        if (np2srv_sr_session_set_options(sessions->srs, sessions->opts ^ SR_SESS_CONFIG_ONLY, &ereply)) {
             goto error;
         }
-        sessions->opts ^= config_only;
+        sessions->opts ^= SR_SESS_CONFIG_ONLY;
     }
 
     /* create filters */