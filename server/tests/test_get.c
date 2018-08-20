CESNET/netopeer2

commit 6ab061774582d72ed478c3364aedd605be9e104d

server OPTIMIZE cache creating data tree from sysrepo
-> Optimize transforming sr_val_t to struct lyd_node.

server/op_get_config.c
@@ -36,6 +36,7 @@ opget_build_subtree_from_sysrepo(sr_session_ctx_t *srs, struct lyd_node **root, 
     sr_val_t *value;
     sr_val_iter_t *sriter;
     struct lyd_node *node;
+    struct sr2ly_cache cache;
     char *full_subtree_xpath = NULL;
     int rc;
 
@@ -44,6 +45,7 @@ opget_build_subtree_from_sysrepo(sr_session_ctx_t *srs, struct lyd_node **root, 
         return -1;
     }
 
+    memset(&cache, 0, sizeof cache);
     np2srv_sr_session_refresh(srs, NULL);
 
     rc = np2srv_sr_get_items_iter(srs, full_subtree_xpath, &sriter, NULL);
@@ -56,7 +58,7 @@ opget_build_subtree_from_sysrepo(sr_session_ctx_t *srs, struct lyd_node **root, 
     }
 
     while ((!np2srv_sr_get_item_next(srs, sriter, &value, NULL))) {
-        if (op_sr_val_to_lyd_node(*root, value, &node)) {
+        if (op_sr2ly(*root, value, &node, &cache)) {
             sr_free_val(value);
             sr_free_val_iter(sriter);
             return -1;
@@ -69,6 +71,7 @@ opget_build_subtree_from_sysrepo(sr_session_ctx_t *srs, struct lyd_node **root, 
     }
     sr_free_val_iter(sriter);
 
+    op_sr2ly_free_cache(&cache);
     return 0;
 }
 

server/op_notifications.c
@@ -190,8 +190,11 @@ np2srv_ntf_clb(const sr_ev_notif_type_t notif_type, const char *xpath, const sr_
 {
     struct np_subscriber *subscriber = (struct np_subscriber *)private_ctx;
     struct lyd_node *ntf = NULL, *node;
+    struct sr2ly_cache cache;
     size_t i;
     const char *ntf_type_str = NULL;
+
+    memset(&cache, 0, sizeof cache);
 
     switch (notif_type) {
     case SR_EV_NOTIF_T_REALTIME:
@@ -219,7 +222,7 @@ np2srv_ntf_clb(const sr_ev_notif_type_t notif_type, const char *xpath, const sr_
         }
 
         for (i = 0; i < val_cnt; i++) {
-            if (op_sr_val_to_lyd_node(ntf, &vals[i], &node)) {
+            if (op_sr2ly(ntf, &vals[i], &node, &cache)) {
                 ERR("Creating notification (%s) data (%s) failed.", xpath, vals[i].xpath);
                 goto error;
             }
@@ -230,6 +233,7 @@ np2srv_ntf_clb(const sr_ev_notif_type_t notif_type, const char *xpath, const sr_
     np2srv_ntf_send(subscriber, ntf, timestamp, notif_type);
 
 error:
+    op_sr2ly_free_cache(&cache);
     lyd_free(ntf);
 }
 

server/operations.c
@@ -2630,25 +2630,299 @@ op_filter_create(struct lyd_node *filter_node, char ***filters, int *filter_coun
     return 0;
 }
 
-int
-op_sr_val_to_lyd_node(struct lyd_node *root, const sr_val_t *sr_val, struct lyd_node **new_node)
+static const char *
+op_sr2ly_parse_node(const char *xpath, const char **mod, int *mod_len, const char **name, int *name_len,
+                    const char **pred, int *pred_len)
 {
-    char numstr[22];
-    char *str;
-
-    str = op_get_srval(np2srv.ly_ctx, sr_val, numstr);
-    if (!str) {
-        str = "";
+    if (xpath[0] != '/') {
+        return NULL;
     }
 
-    ly_errno = LY_SUCCESS;
+    /* parse module name */
+    *mod = xpath + 1;
+    *mod_len = 0;
+    while (((*mod)[*mod_len] != ':') && ((*mod)[*mod_len] != '/') && ((*mod)[*mod_len] != '[')
+            && ((*mod)[*mod_len] != '\0')) {
+        ++(*mod_len);
+    }
+    if ((*mod)[*mod_len] != ':') {
+        /* no module name, this is the node name */
+        *name = *mod;
+        *mod = NULL;
+        *name_len = *mod_len;
+        *mod_len = 0;
+    } else {
+        /* parse node name */
+        *name = *mod + *mod_len + 1;
+        *name_len = 0;
+        while (((*name)[*name_len] != '/') && ((*name)[*name_len] != '[') && ((*name)[*name_len] != '\0')) {
+            ++(*name_len);
+        }
+    }
 
-    *new_node = lyd_new_path(root, np2srv.ly_ctx, sr_val->xpath, str,
-            (sr_val->type == SR_ANYXML_T || sr_val->type == SR_ANYDATA_T) ? LYD_ANYDATA_SXML : 0,
-            LYD_PATH_OPT_UPDATE | (sr_val->dflt ? LYD_PATH_OPT_DFLT : 0));
-    if (ly_errno) {
+    /* parse all predicates */
+    if ((*name)[*name_len] == '[') {
+        *pred = *name + *name_len;
+        *pred_len = 0;
+        do {
+            while ((*pred)[*pred_len] != ']') {
+                ++(*pred_len);
+            }
+            ++(*pred_len);
+        } while ((*pred)[*pred_len] == '[');
+
+        xpath = *pred + *pred_len;
+    } else {
+        *pred = NULL;
+        *pred_len = 0;
+
+        xpath = *name + *name_len;
+    }
+
+    return xpath;
+}
+
+static void
+op_sr2ly_add_cache(struct sr2ly_cache *cache, const char *mod, int mod_len, const char *name, int name_len,
+                   const char *pred, int pred_len)
+{
+    if (cache->used == cache->size) {
+        ++cache->size;
+        cache->items = realloc(cache->items, cache->size * sizeof *cache->items);
+    }
+    ++cache->used;
+    cache->items[cache->used - 1].node = NULL;
+    cache->items[cache->used - 1].mod = mod ? strndup(mod, mod_len) : NULL;
+    cache->items[cache->used - 1].name = strndup(name, name_len);
+    cache->items[cache->used - 1].pred = pred ? strndup(pred, pred_len) : NULL;
+}
+
+static int
+op_sr2ly_get_cache_parent(const char *xpath, struct sr2ly_cache *cache, struct lyd_node *root, int *new_node_i)
+{
+    const char *mod, *name, *pred;
+    char *path;
+    const struct lys_module *module;
+    size_t i;
+    int mod_len, name_len, pred_len;
+    struct ly_set *set;
+
+    /* look for the parent in the cache */
+    for (i = 0; i < cache->used; ++i) {
+        xpath = op_sr2ly_parse_node(xpath, &mod, &mod_len, &name, &name_len, &pred, &pred_len);
+
+        /* node name */
+        if (strncmp(name, cache->items[i].name, name_len) || cache->items[i].name[name_len]) {
+            break;
+        }
+
+        /* node module name */
+        if ((mod && !cache->items[i].mod) || (!mod && cache->items[i].mod)) {
+            break;
+        }
+        if (mod && (strncmp(mod, cache->items[i].mod, mod_len) || cache->items[i].mod[mod_len])) {
+            break;
+        }
+
+        /* node predicates */
+        if ((pred && !cache->items[i].pred) || (!pred && cache->items[i].pred)) {
+            /* should not ever happen, actually */
+            break;
+        }
+        if (pred && (strncmp(pred, cache->items[i].pred, pred_len) || cache->items[i].pred[pred_len])) {
+            break;
+        }
+
+        /* we have matched, try to find the next match */
+    }
+
+    if (i < cache->used) {
+        /* remove unused items */
+        while (cache->used > i) {
+            free(cache->items[cache->used - 1].name);
+            free(cache->items[cache->used - 1].mod);
+            free(cache->items[cache->used - 1].pred);
+
+            --cache->used;
+        }
+    } else if (xpath[0]) {
+        /* all parsed nodes matched, get the next one */
+        xpath = op_sr2ly_parse_node(xpath, &mod, &mod_len, &name, &name_len, &pred, &pred_len);
+    }
+
+    /* some parents did not match, either the parent does not exist or is just not cached */
+    while (xpath[0]) {
+        module = NULL;
+        set = NULL;
+        if (root) {
+            /* build path to find */
+            asprintf(&path, "%s%.*s%s%.*s%.*s", i ? "" : "/", mod_len, mod ? mod : "", mod ? ":" : "", name_len, name,
+                     pred_len, pred ? pred : "");
+            if (cache->used) {
+                /* relative from parent */
+                set = lyd_find_path(cache->items[cache->used - 1].node, path);
+            } else {
+                /* top-level node */
+                set = lyd_find_path(root, path);
+            }
+            free(path);
+
+            if (!set || (set->number > 1)) {
+                ly_set_free(set);
+                return -1;
+            }
+        }
+
+        /* create new item for this parent */
+        op_sr2ly_add_cache(cache, mod, mod_len, name, name_len, pred, pred_len);
+
+        if (!root || !set->number) {
+            /* create the parent node first, it does not exist */
+            if (cache->items[cache->used - 1].mod) {
+                module = ly_ctx_get_module(np2srv.ly_ctx, cache->items[cache->used - 1].mod, NULL, 1);
+                if (!module) {
+                    ly_set_free(set);
+                    return -1;
+                }
+            }
+
+            /* it is a parent, must be list or container (or action, notif, whatever) */
+            if (cache->used > 1) {
+                /* inner node */
+                cache->items[cache->used - 1].node = lyd_new(cache->items[cache->used - 2].node, module, cache->items[cache->used - 1].name);
+            } else {
+                /* top-level node */
+                cache->items[cache->used - 1].node = lyd_new(NULL, module, cache->items[cache->used - 1].name);
+            }
+            if (!cache->items[cache->used - 1].node) {
+                ly_set_free(set);
+                return -1;
+            }
+            if (*new_node_i == -1) {
+                /* first created node */
+                *new_node_i = cache->used - 1;
+            }
+        } else {
+            /* the parent exists, cache it */
+            cache->items[cache->used - 1].node = set->set.d[0];
+        }
+        ly_set_free(set);
+
+        /* parse next node */
+        xpath = op_sr2ly_parse_node(xpath, &mod, &mod_len, &name, &name_len, &pred, &pred_len);
+    }
+
+    /* create item for the newly created node and return its index */
+    op_sr2ly_add_cache(cache, mod, mod_len, name, name_len, pred, pred_len);
+    return 0;
+}
+
+int
+op_sr2ly(struct lyd_node *root, const sr_val_t *sr_val, struct lyd_node **new_node, struct sr2ly_cache *cache)
+{
+    char numstr[22];
+    char *val_str;
+    int new_node_i = -1;
+    struct lyd_node *parent = NULL, *node;
+    const struct lys_module *mod = NULL;
+
+    /* last used index in cache is ours, we can learn everything based on that */
+    if (op_sr2ly_get_cache_parent(sr_val->xpath, cache, root, &new_node_i)) {
         return -1;
     }
 
+    /* get module */
+    if (cache->items[cache->used - 1].mod) {
+        mod = ly_ctx_get_module(np2srv.ly_ctx, cache->items[cache->used - 1].mod, NULL, 1);
+        if (!mod) {
+            return -1;
+        }
+    }
+
+    /* get parent */
+    if (cache->used > 1) {
+        parent = cache->items[cache->used - 2].node;
+    }
+
+    /* create the new node */
+    switch (sr_val->type) {
+    case SR_STRING_T:
+    case SR_BINARY_T:
+    case SR_BITS_T:
+    case SR_ENUM_T:
+    case SR_IDENTITYREF_T:
+    case SR_INSTANCEID_T:
+    case SR_LEAF_EMPTY_T:
+    case SR_BOOL_T:
+    case SR_DECIMAL64_T:
+    case SR_UINT8_T:
+    case SR_UINT16_T:
+    case SR_UINT32_T:
+    case SR_UINT64_T:
+    case SR_INT8_T:
+    case SR_INT16_T:
+    case SR_INT32_T:
+    case SR_INT64_T:
+        val_str = op_get_srval(np2srv.ly_ctx, sr_val, numstr);
+        node = lyd_new_leaf(parent, mod, cache->items[cache->used - 1].name, val_str);
+        break;
+    case SR_ANYDATA_T:
+    case SR_ANYXML_T:
+        val_str = op_get_srval(np2srv.ly_ctx, sr_val, numstr);
+        node = lyd_new_anydata(parent, mod, cache->items[cache->used - 1].name, val_str, LYD_ANYDATA_SXML);
+        break;
+    case SR_LIST_T:
+    case SR_CONTAINER_T:
+    case SR_CONTAINER_PRESENCE_T:
+        node = lyd_new(parent, mod, cache->items[cache->used - 1].name);
+        break;
+    default:
+        return -1;
+    }
+    if (!node) {
+        return -1;
+    }
+
+    /* inherit dflt flag */
+    node->dflt = sr_val->dflt;
+
+    /* insert into data tree if required */
+    if (!parent && root) {
+        if (lyd_insert_after(root->prev, node)) {
+            return -1;
+        }
+    }
+
+    if (new_node_i == -1) {
+        /* this is the first created node, return it */
+        *new_node = node;
+    } else {
+        /* return the parent created before */
+        *new_node = cache->items[new_node_i].node;
+    }
+
+    if (node->schema->nodetype & (LYS_CONTAINER | LYS_LIST)) {
+        /* store in cache */
+        cache->items[cache->used - 1].node = node;
+    } else {
+        /* useless to store ending nodes in cache */
+        free(cache->items[cache->used - 1].mod);
+        free(cache->items[cache->used - 1].name);
+        free(cache->items[cache->used - 1].pred);
+        --cache->used;
+    }
     return 0;
+}
+
+void
+op_sr2ly_free_cache(struct sr2ly_cache *cache)
+{
+    size_t i;
+
+    for (i = 0; i < cache->used; ++i) {
+        free(cache->items[i].mod);
+        free(cache->items[i].name);
+        free(cache->items[i].pred);
+    }
+    free(cache->items);
 }

server/operations.h
@@ -141,7 +141,20 @@ struct nc_server_reply *op_build_err_nacm(struct nc_server_reply *ereply);
 int op_filter_get_tree_from_data(struct lyd_node **root, struct lyd_node *data, const char *subtree_path);
 int op_filter_xpath_add_filter(char *new_filter, char ***filters, int *filter_count);
 int op_filter_create(struct lyd_node *filter_node, char ***filters, int *filter_count);
-int op_sr_val_to_lyd_node(struct lyd_node *root, const sr_val_t *sr_val, struct lyd_node **new_node);
+
+struct sr2ly_cache {
+    struct {
+        struct lyd_node *node;
+        char *name;
+        char *mod;
+        char *pred;
+    } *items;
+    size_t used;
+    size_t size;
+};
+
+void op_sr2ly_free_cache(struct sr2ly_cache *cache);
+int op_sr2ly(struct lyd_node *root, const sr_val_t *sr_val, struct lyd_node **new_node, struct sr2ly_cache *cache);
 
 struct nc_server_reply *op_get(struct lyd_node *rpc, struct nc_session *ncs);
 struct nc_server_reply *op_lock(struct lyd_node *rpc, struct nc_session *ncs);
@@ -159,6 +172,5 @@ struct nc_server_reply *op_ntf_subscribe(struct lyd_node *rpc, struct nc_session
 void op_ntf_unsubscribe(struct nc_session *session);
 void op_ntf_yang_lib_change(const struct lyd_node *ylib_info);
 struct lyd_node *ntf_get_data(void);
-
 
 #endif /* NP2SRV_OPERATIONS_H_ */

commit 429e60dcd439ee84bdec6e74345e4bbc0a5a7da7

Add subtree filter unit test
This commit adds a more complex subtree filter test in `test_get`. The new
test demonstrates a failure introduced in 6ab0617.

When a get request is sent with the following filter:

```
<filter xmlns="urn:ietf:params:xml:ns:netconf:base:1.0">
  <yang-library xmlns="urn:ietf:params:xml:ns:yang:ietf-yang-library">
    <module-set>
      <name>complete</name>
      <module>
        <name>ietf-yang-library</name>
        <revision/>
      </module>
    </module-set>
  </yang-library>
</filter>
```

The following subtree should be returned:

```
<data xmlns="urn:ietf:params:xml:ns:netconf:base:1.0">
  <yang-library xmlns="urn:ietf:params:xml:ns:yang:ietf-yang-library">
    <module-set>
      <name>complete</name>
      <module>
        <name>ietf-yang-library</name>
        <revision>2018-01-17</revision>
      </module>
    </module-set>
  </yang-library>
</data>
```

However, beginning in 6ab0617, the following malformed tree is returned:

```
<data xmlns="urn:ietf:params:xml:ns:netconf:base:1.0">
  <yang-library xmlns="urn:ietf:params:xml:ns:yang:ietf-yang-library">
    <module-set>
      <name>complete</name>
    </module-set>
    <module-set>
      <module>
        <name>ietf-yang-library</name>
      </module>
    </module-set>
    <module-set>
      <module>
        <revision>2018-01-17</revision>
      </module>
    </module-set>
  </yang-library>
</data>
```

Notice the erroneous extra termination nodes.

This test passes in commits prior to 6ab0617. It will fail beginning in
6ab0617.

server/tests/test_get.c
@@ -149,10 +149,18 @@ __wrap_sr_get_item_next(sr_session_ctx_t *session, sr_val_iter_t *iter, sr_val_t
     char *path;
     (void)session;
 
+    /* Accept any queries in the ietf-yang-library namespace. */
     if (!strncmp(xpath, "/ietf-yang-library:", 19)) {
         if (!set) {
             root = ly_ctx_info(np2srv.ly_ctx);
-            set = lyd_find_path(root, "//.");
+            /* Our test data only has information from the yang-library container,
+               so we can only service requests inside that container. But if the caller
+               has specified a more restrictive path inside yang-library, as in the case
+               of the filter tests, then use it. */
+            if (strncmp(xpath, "/ietf-yang-library:yang-library", 31)) {
+                xpath = "/ietf-yang-library:yang-library//.";
+            }
+            set = lyd_find_path(root, xpath);
         }
 
         if (!set->number) {
@@ -514,7 +522,7 @@ test_get(void **state)
 }
 
 static void
-test_get_filter(void **state)
+test_get_filter1(void **state)
 {
     (void)state; /* unused */
     const char *get_rpc =
@@ -533,12 +541,61 @@ test_get_filter(void **state)
     test_read(p_in, get_rpl, __LINE__);
 }
 
+static void
+test_get_filter2(void **state)
+{
+    (void)state; /* unused */
+    const char *get_rpc =
+    "<rpc msgid=\"2\" xmlns=\"urn:ietf:params:xml:ns:netconf:base:1.0\">"
+        "<get>"
+            "<filter type=\"subtree\">"
+                "<yang-library xmlns=\"urn:ietf:params:xml:ns:yang:ietf-yang-library\">"
+                    "<module-set>"
+                        "<name>complete</name>"
+                        "<module>"
+                            "<name>ietf-yang-library</name>"
+                            "<revision/>"
+                        "</module>"
+                    "</module-set>"
+                "</yang-library>"
+            "</filter>"
+        "</get>"
+    "</rpc>";
+    const char *get_rpl =
+    "<rpc-reply msgid=\"2\" xmlns=\"urn:ietf:params:xml:ns:netconf:base:1.0\">"
+        "<data xmlns=\"urn:ietf:params:xml:ns:netconf:base:1.0\">"
+            "<yang-library xmlns=\"urn:ietf:params:xml:ns:yang:ietf-yang-library\">"
+                "<module-set>"
+                    "<name>complete</name>"
+                    "<module>"
+                        "<name>ietf-yang-library</name>"
+                        "<revision>2018-01-17</revision>"
+                    "</module>"
+                "</module-set>"
+            "</yang-library>"
+        "</data>"
+    "</rpc-reply>";
+
+    test_write(p_out, get_rpc, __LINE__);
+    test_read(p_in, get_rpl, __LINE__);
+}
+
+static void
+test_startstop(void **state)
+{
+    (void)state; /* unused */
+    return;
+}
+
 int
 main(void)
 {
     const struct CMUnitTest tests[] = {
-                    cmocka_unit_test_setup(test_get, np_start),
-                    cmocka_unit_test_teardown(test_get_filter, np_stop),
+                    cmocka_unit_test_setup(test_startstop, np_start),
+                    cmocka_unit_test(test_get),
+                    cmocka_unit_test(test_get_filter1),
+                    cmocka_unit_test(test_get_filter2),
+                    cmocka_unit_test_teardown(test_startstop, np_stop),
     };
 
     if (setenv("CMOCKA_TEST_ABORT", "1", 1)) {