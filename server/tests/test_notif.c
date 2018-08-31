CESNET/netopeer2

commit e75e3391ca46faf0a02d73c985671ed04a408a85

server BUGFIX creating keys in case sysrepo will not return them
Also a test case added that did not pass before.
Fixes #329

server/operations.c
@@ -2631,6 +2631,85 @@ op_filter_create(struct lyd_node *filter_node, char ***filters, int *filter_coun
     return 0;
 }
 
+static int
+op_sr2ly_parse_pred(const char **pred, char **name, char **value)
+{
+    int len;
+    char quote;
+
+    *name = NULL;
+    *value = NULL;
+
+    if ((*pred)[0] != '[') {
+        goto error;
+    }
+    ++(*pred);
+
+    for (len = 0; (*pred)[len] != '='; ++len);
+
+    /* copy node name */
+    *name = strndup(*pred, len);
+
+    *pred += len;
+
+    ++(*pred);
+
+    if (((*pred)[0] != '\'') && ((*pred)[0] != '\"')) {
+        goto error;
+    }
+
+    quote = (*pred)[0];
+    ++(*pred);
+
+    for (len = 0; (*pred)[len] != quote; ++len);
+
+    /* copy value */
+    *value = strndup(*pred, len);
+
+    *pred += len;
+
+    ++(*pred);
+
+    if ((*pred)[0] != ']') {
+        goto error;
+    }
+    ++(*pred);
+
+    if ((*pred)[0] != '[') {
+        /* no more predicates */
+        *pred = NULL;
+    }
+
+    return 0;
+
+error:
+    free(*name);
+    free(*value);
+    return -1;
+}
+
+static int
+op_sr2ly_create_keys(const char *pred, struct lyd_node *parent, const struct lys_module *module)
+{
+    char *name, *value;
+    struct lyd_node *node;
+
+    while (pred) {
+        if (op_sr2ly_parse_pred(&pred, &name, &value)) {
+            return -1;
+        }
+
+        node = lyd_new_leaf(parent, module, name, value);
+        free(name);
+        free(value);
+        if (!node) {
+            return -1;
+        }
+    }
+
+    return 0;
+}
+
 static const char *
 op_sr2ly_parse_node(const char *xpath, const char **mod, int *mod_len, const char **name, int *name_len,
                     const char **pred, int *pred_len)
@@ -2803,6 +2882,12 @@ op_sr2ly_get_cache_parent(const char *xpath, struct sr2ly_cache *cache, struct l
                 /* first created node */
                 *new_node_i = cache->used - 1;
             }
+
+            /* create list keys if possible */
+            if (op_sr2ly_create_keys(pred, cache->items[cache->used - 1].node, module)) {
+                ly_set_free(set);
+                return -1;
+            }
         } else {
             /* the parent exists, cache it */
             cache->items[cache->used - 1].node = set->set.d[0];
@@ -2816,7 +2901,7 @@ op_sr2ly_get_cache_parent(const char *xpath, struct sr2ly_cache *cache, struct l
         xpath = op_sr2ly_parse_node(xpath, &mod, &mod_len, &name, &name_len, &pred, &pred_len);
     }
 
-    /* create item for the newly created node and return its index */
+    /* create item for the newly created node */
     op_sr2ly_add_cache(cache, mod, mod_len, name, name_len, pred, pred_len);
     return 0;
 }
@@ -2826,13 +2911,38 @@ op_sr2ly(struct lyd_node *root, const sr_val_t *sr_val, struct lyd_node **new_no
 {
     char numstr[22];
     char *val_str;
+    uint16_t i;
     int new_node_i = -1, parent_dflt_depth = 0;
     struct lyd_node *parent = NULL, *node, *iter;
     const struct lys_module *mod = NULL;
+    struct lys_node_list *list;
 
     /* last used index in cache is ours, we can learn everything based on that */
     if (op_sr2ly_get_cache_parent(sr_val->xpath, cache, root, &new_node_i)) {
         return -1;
+    }
+
+    /* if we wanted to create a list key, it exists already */
+    if (!cache->items[cache->used - 1].mod && cache->items[cache->used - 2].pred) {
+        assert(cache->items[cache->used - 2].node->schema->nodetype == LYS_LIST);
+
+        list = (struct lys_node_list *)cache->items[cache->used - 2].node->schema;
+        for (i = 0; i < list->keys_size; ++i) {
+            if (!strcmp(list->keys[i]->name, cache->items[cache->used - 1].name)) {
+                break;
+            }
+        }
+
+        if (i < list->keys_size) {
+            if (new_node_i == -1) {
+                /* this is not the first created node, we can return whatever */
+                *new_node = NULL;
+            } else {
+                /* return the list (or its parent) as we correctly should */
+                *new_node = cache->items[new_node_i].node;
+            }
+            goto key_created;
+        }
     }
 
     /* get module */
@@ -2896,6 +3006,13 @@ op_sr2ly(struct lyd_node *root, const sr_val_t *sr_val, struct lyd_node **new_no
     /* inherit dflt flag */
     node->dflt = sr_val->dflt;
 
+    if (sr_val->type == SR_LIST_T) {
+        /* create also all the keys of a list */
+        if (op_sr2ly_create_keys(cache->items[cache->used - 1].pred, node, mod)) {
+            return -1;
+        }
+    }
+
     /* Restore the parent(s)' default flags as well */
     for (iter = parent; iter && parent_dflt_depth; iter = iter->parent, --parent_dflt_depth) {
         iter->dflt = 1;
@@ -2920,6 +3037,7 @@ op_sr2ly(struct lyd_node *root, const sr_val_t *sr_val, struct lyd_node **new_no
         /* store in cache */
         cache->items[cache->used - 1].node = node;
     } else {
+key_created:
         /* useless to store ending nodes in cache */
         free(cache->items[cache->used - 1].mod);
         free(cache->items[cache->used - 1].name);

server/tests/test_get.c
@@ -581,6 +581,33 @@ test_get_filter2(void **state)
 }
 
 static void
+test_get_filter3(void **state)
+{
+    (void)state; /* unused */
+    const char *get_rpc =
+    "<rpc msgid=\"3\" xmlns=\"urn:ietf:params:xml:ns:netconf:base:1.0\">"
+        "<get>"
+            "<filter xmlns:ylib=\"urn:ietf:params:xml:ns:yang:ietf-yang-library\" "
+                "type=\"xpath\" select=\"/ylib:yang-library/module-set[name='complete']/checksum\"/>"
+        "</get>"
+    "</rpc>";
+    const char *get_rpl =
+    "<rpc-reply msgid=\"3\" xmlns=\"urn:ietf:params:xml:ns:netconf:base:1.0\">"
+        "<data xmlns=\"urn:ietf:params:xml:ns:netconf:base:1.0\">"
+            "<yang-library xmlns=\"urn:ietf:params:xml:ns:yang:ietf-yang-library\">"
+                "<module-set>"
+                    "<name>complete</name>"
+                    "<checksum>23</checksum>"
+                "</module-set>"
+            "</yang-library>"
+        "</data>"
+    "</rpc-reply>";
+
+    test_write(p_out, get_rpc, __LINE__);
+    test_read(p_in, get_rpl, __LINE__);
+}
+
+static void
 test_startstop(void **state)
 {
     (void)state; /* unused */
@@ -595,6 +622,7 @@ main(void)
                     cmocka_unit_test(test_get),
                     cmocka_unit_test(test_get_filter1),
                     cmocka_unit_test(test_get_filter2),
+                    cmocka_unit_test(test_get_filter3),
                     cmocka_unit_test_teardown(test_startstop, np_stop),
     };
 

commit 170a2ee6188db06a1bb228358ac4c116625eff87

Fix list without key
This commit fixes the scenario in which the server needs to report a list
without keys. (This is valid for lists that do not represent configuration;
see RFC 7950 section 7.8.2.) This scenario was broken by the changes in
e75e339, which caused `op_sr2ly()` to fail if the list's xpath predicate
did not contain a `child="value"` expression.

I made two modifications to `op_sr2ly_parse_pred()`:
1. Fix the loop that searches for `'='` so that it stops at the NULL
termination of the string if no `'='` has yet been found. (The existing code
could search far beyond the bounds of the string, sometimes resulting in a
segmentation fault.)
2. Distinguish between a failure to find a `child="value"` predicate, which
is a valid case, and other general parsing errors, which are not.

I then changed `op_sr2ly_create_keys()` so that it no longer fails if no list
keys are found.

I also added a test case that demonstrates the problem. I simulated the
`netconf-config-change` notification from RFC 6470, which contains a list
without a key. The test case will pass in commits prior to e75e339, fail
with e75e339, and pass again with this patch applied.

server/operations.c
@@ -2631,11 +2631,18 @@ op_filter_create(struct lyd_node *filter_node, char ***filters, int *filter_coun
     return 0;
 }
 
+enum {
+    OP_SR2LY_PARSE_PRED_SUCCESS = 0,
+    OP_SR2LY_PARSE_PRED_NO_KEY,
+    OP_SR2LY_PARSE_PRED_PARSE_ERROR
+};
+
 static int
 op_sr2ly_parse_pred(const char **pred, char **name, char **value)
 {
     int len;
     char quote;
+    int ret = OP_SR2LY_PARSE_PRED_PARSE_ERROR;
 
     *name = NULL;
     *value = NULL;
@@ -2645,7 +2652,12 @@ op_sr2ly_parse_pred(const char **pred, char **name, char **value)
     }
     ++(*pred);
 
-    for (len = 0; (*pred)[len] != '='; ++len);
+    for (len = 0; (*pred)[len] && (*pred)[len] != '='; ++len);
+
+    if ((*pred)[len] != '=') {
+        ret = OP_SR2LY_PARSE_PRED_NO_KEY;
+        goto error;
+    }
 
     /* copy node name */
     *name = strndup(*pred, len);
@@ -2680,12 +2692,12 @@ op_sr2ly_parse_pred(const char **pred, char **name, char **value)
         *pred = NULL;
     }
 
-    return 0;
+    return OP_SR2LY_PARSE_PRED_SUCCESS;
 
 error:
     free(*name);
     free(*value);
-    return -1;
+    return ret;
 }
 
 static int
@@ -2693,10 +2705,14 @@ op_sr2ly_create_keys(const char *pred, struct lyd_node *parent, const struct lys
 {
     char *name, *value;
     struct lyd_node *node;
+    int result;
 
     while (pred) {
-        if (op_sr2ly_parse_pred(&pred, &name, &value)) {
-            return -1;
+        result = op_sr2ly_parse_pred(&pred, &name, &value);
+        if (result != OP_SR2LY_PARSE_PRED_SUCCESS) {
+            /* It is legal for non-config lists to contain no keys,
+               so do not return a failure if no keys are found */
+            return result == OP_SR2LY_PARSE_PRED_NO_KEY ? 0 : -1;
         }
 
         node = lyd_new_leaf(parent, module, name, value);

server/tests/test_notif.c
@@ -371,6 +371,81 @@ test_basic(void **state)
 }
 
 static void
+test_config_change(void **state)
+{
+    (void)state; /* unused */
+    sr_val_t *vals;
+    size_t val_cnt;
+    const char *subsc_rpc =
+    "<rpc msgid=\"1\" xmlns=\"urn:ietf:params:xml:ns:netconf:base:1.0\">"
+        "<create-subscription xmlns=\"urn:ietf:params:xml:ns:netconf:notification:1.0\">"
+            "<stream>NETCONF</stream>"
+        "</create-subscription>"
+    "</rpc>";
+    const char *subsc_rpl =
+    "<rpc-reply msgid=\"1\" xmlns=\"urn:ietf:params:xml:ns:netconf:base:1.0\">"
+        "<ok/>"
+    "</rpc-reply>";
+    const char *notif_data =
+    "<notification xmlns=\"urn:ietf:params:xml:ns:netconf:notification:1.0\">"
+        "<eventTime>0000-00-00T00:00:00Z</eventTime>"
+        "<netconf-config-change xmlns=\"urn:ietf:params:xml:ns:yang:ietf-netconf-notifications\">"
+          "<changed-by>"
+            "<session-id>42</session-id>"
+            "<username>test</username>"
+          "</changed-by>"
+          "<datastore>running</datastore>"
+          "<edit>"
+            "<operation>create</operation>"
+          "</edit>"
+        "</netconf-config-change>"
+    "</notification>";
+
+    initialized = 0;
+    while (!initialized) {
+        usleep(100000);
+    }
+
+    test_write(p_out, subsc_rpc, __LINE__);
+    test_read(p_in, subsc_rpl, __LINE__);
+
+    /* send notif */
+    val_cnt = 6;
+    vals = calloc(val_cnt, sizeof *vals);
+
+    vals[0].xpath = strdup("/ietf-netconf-notifications:netconf-config-change/changed-by");
+    vals[0].type = SR_CONTAINER_T;
+
+    vals[1].xpath = strdup("/ietf-netconf-notifications:netconf-config-change/changed-by/session-id");
+    vals[1].type = SR_UINT32_T;
+    vals[1].data.uint32_val = 42;
+
+    vals[2].xpath = strdup("/ietf-netconf-notifications:netconf-config-change/changed-by/username");
+    vals[2].type = SR_STRING_T;
+    vals[2].data.string_val = strdup("test");
+
+    vals[3].xpath = strdup("/ietf-netconf-notifications:netconf-config-change/datastore");
+    vals[3].type = SR_ENUM_T;
+    vals[3].data.enum_val = strdup("running");
+
+    vals[4].xpath = strdup("/ietf-netconf-notifications:netconf-config-change/edit[1]");
+    vals[4].type = SR_LIST_T;
+
+    /* A real config-change notification will have an instance-id here, which is omitted for ease of testing. */
+
+    vals[5].xpath = strdup("/ietf-netconf-notifications:netconf-config-change/edit[1]/operation");
+    vals[5].type = SR_ENUM_T;
+    vals[5].data.enum_val = strdup("create");
+
+    notif_clb(SR_EV_NOTIF_T_REALTIME, "/ietf-netconf-notifications:netconf-config-change", vals, val_cnt, time(NULL), notif_clb_data);
+
+    sr_free_values(vals, val_cnt);
+
+    /* read notif */
+    test_read(p_in, notif_data, __LINE__);
+}
+
+static void
 test_filter_xpath(void **state)
 {
     (void)state; /* unused */
@@ -630,6 +705,7 @@ main(void)
 {
     const struct CMUnitTest tests[] = {
                     cmocka_unit_test_setup(test_basic, np_start),
+                    cmocka_unit_test(test_config_change),
                     cmocka_unit_test(test_filter_xpath),
                     cmocka_unit_test(test_filter_subtree),
                     cmocka_unit_test_teardown(test_replay, np_stop),