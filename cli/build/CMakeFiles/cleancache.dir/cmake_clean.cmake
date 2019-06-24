a1ba915aef2c233e07211659f77ce2ead3ddb64c
Michal Vasko <mvasko@cesnet.cz>
2019-06-24 19:22:02 +0200

    cli MAINTENANCE incorrectly commited build files

--- cli/build/CMakeFiles/cleancache.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/cleancache"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/cleancache.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
