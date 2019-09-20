83e4d57905422b27f4afd6d95c438bae96c71f03
Michal Vasko <mvasko@cesnet.cz>
2019-09-20 15:22:22 +0200

    server BUILD hide invalid warnings when installing modules

--- server/setup.sh
+++ server/setup.sh
@@ -33,4 +33,4 @@ $SYSREPOCTL -i $MODDIR/ietf-truststore@2019-07-02.yang -e truststore-supported -
 $SYSREPOCTL -i $MODDIR/ietf-tcp-common@2019-07-02.yang -e keepalives-supported -s $MODDIR
 $SYSREPOCTL -i $MODDIR/ietf-ssh-server@2019-07-02.yang -e local-client-auth-supported -s $MODDIR
 $SYSREPOCTL -i $MODDIR/ietf-tls-server@2019-07-02.yang -e local-client-auth-supported -s $MODDIR
-$SYSREPOCTL -i $MODDIR/ietf-netconf-server@2019-07-02.yang -e ssh-listen -e tls-listen -e ssh-call-home -e tls-call-home -s $MODDIR
+$SYSREPOCTL -i $MODDIR/ietf-netconf-server@2019-07-02.yang -e ssh-listen -e tls-listen -e ssh-call-home -e tls-call-home -s $MODDIR -v2
