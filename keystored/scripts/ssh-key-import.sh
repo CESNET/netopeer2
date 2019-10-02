f5a5ea2ae913246e5364113a43cc2ff96281d587
Robin Jarry <robin.jarry@6wind.com>
2019-10-02 17:46:09 +0200

    keystored: force public key in pem format
    Fix the following error:
    
     [ERR] File "/etc/keystored/keys/ssh_host_rsa_key.pem.pub" is not
     a public key in PEM format.
    
    ssh-keygen ignores the "-m pem" argument for the public key file. It
    always generates it in the openssh format.
    
    Convert it to PEM afterwards.
    
    ssh-keygen requires that the converted file has 0600 permissions (it
    assumes it is a private key). Restrict permissions before converting and
    restore them afterwards.
    
    Fixes: 1ad19f2c1fc4 ("keystored CHANGE generate new SSH hostkey, do not use the OpenSSH one")
    Signed-off-by: Robin Jarry <robin.jarry@6wind.com>

--- keystored/scripts/ssh-key-import.sh
+++ keystored/scripts/ssh-key-import.sh
@@ -28,8 +28,14 @@ else
     else
         echo "- SSH hostkey not found, generating a new one..."
         $SSH_KEYGEN -m pem -t rsa -q -N "" -f ${KEYSTORED_KEYS_DIR}/ssh_host_rsa_key.pem
+        # public key format cannot be specified, it is always in the openssh format
+        # force convert it to PEM as well
+        $CHMOD go-rw ${KEYSTORED_KEYS_DIR}/ssh_host_rsa_key.pem.pub
+        $SSH_KEYGEN -e -m pem -f ${KEYSTORED_KEYS_DIR}/ssh_host_rsa_key.pem.pub > ${KEYSTORED_KEYS_DIR}/pub.pem
+        mv -f ${KEYSTORED_KEYS_DIR}/pub.pem ${KEYSTORED_KEYS_DIR}/ssh_host_rsa_key.pem.pub
     fi
     $CHMOD go-rw ${KEYSTORED_KEYS_DIR}/ssh_host_rsa_key.pem
+    $CHMOD go+r ${KEYSTORED_KEYS_DIR}/ssh_host_rsa_key.pem.pub
     echo "- Importing ietf-keystore stock key configuration..."
     $SYSREPOCFG -d startup -i ${STOCK_KEY_CONFIG} ietf-keystore
 fi
