33751549b3c5b5802077fe87e4f3e9e8d160f2cf
Jan Kundrát <jan.kundrat@cesnet.cz>
2020-06-03 10:40:17 +0200

    Allow moving list/leaf-list items to the last position
    The code wanted to deliberately ignore any SR_MOVE_LAST values. I think
    that this happened because of that C convention for adding a FOO_LAST
    items to enums to signify their largest allowed value.
    
    I did not want to modify sysrepo for this as well, so I just added a
    random garbage sentinel for this.
    
    Bug: https://github.com/CESNET/netopeer2/issues/636

--- server/op_editconfig.c
+++ server/op_editconfig.c
@@ -28,6 +28,9 @@
 #include "common.h"
 #include "operations.h"
 
+/* there is no enum for "no move", and the name SR_MOVE_LAST suggests that it might be the last item of an enum */
+#define SR_MOVE_INVALID_SENTINEL 666
+
 static enum NP2_EDIT_OP
 edit_get_op(struct lyd_node *node, enum NP2_EDIT_OP parentop, enum NP2_EDIT_DEFOP defop)
 {
@@ -168,7 +171,7 @@ op_editconfig(struct lyd_node *rpc, struct nc_session *ncs)
     struct nc_server_reply *ereply = NULL;
     struct np2_sessions *sessions = NULL;
     sr_datastore_t ds = 0;
-    sr_move_position_t pos = SR_MOVE_LAST;
+    sr_move_position_t pos = SR_MOVE_INVALID_SENTINEL;
     sr_val_t value;
     struct ly_set *nodeset;
     /* default value for default-operation is "merge" */
@@ -436,7 +439,7 @@ op_editconfig(struct lyd_node *rpc, struct nc_session *ncs)
             }
 
             DBG("EDIT_CONFIG: leaflist %s, operation %s", path, op2str(op[op_index]));
-            if (pos != SR_MOVE_LAST) {
+            if (pos != SR_MOVE_INVALID_SENTINEL) {
                 DBG("EDIT_CONFIG: moving leaflist %s, position %d (%s)", path, pos, rel ? rel : "absolute");
             }
 
@@ -540,10 +543,10 @@ resultcheck:
         }
 
         /* move user-ordered list/leaflist */
-        if (pos != SR_MOVE_LAST) {
+        if (pos != SR_MOVE_INVALID_SENTINEL) {
             ret = np2srv_sr_move_item(sessions->srs, path, pos, rel, &ereply);
             free(rel);
-            pos = SR_MOVE_LAST;
+            pos = SR_MOVE_INVALID_SENTINEL;
             goto resultcheck;
         }
 
