db72898184acba6bdc4b82bff316ac9c6cb7c501
Michal Vasko <mvasko@cesnet.cz>
2021-04-15 10:26:46 +0200

    cmake UPDATE find sysrepo with version

--- CMakeModules/FindSysrepo.cmake
+++ CMakeModules/FindSysrepo.cmake
@@ -1,12 +1,13 @@
 # Find Sysrepo
 #  Once done, it will define
 #
-#  SYSREPO_FOUND - System has SYSREPO
-#  SYSREPO_INCLUDE_DIRS - The SYSREPO include directories
-#  SYSREPO_LIBRARIES - The libraries needed to use SYSREPO
+#  SYSREPO_FOUND - System has sysrepo
+#  SYSREPO_INCLUDE_DIRS - The sysrepo include directories
+#  SYSREPO_LIBRARIES - The libraries needed to use sysrepo
+#  SYSREPO_VERSION - SO version of the found sysrepo library
 #
 #  Author Michal Vasko <mvasko@cesnet.cz>
-#  Copyright (c) 2020 CESNET, z.s.p.o.
+#  Copyright (c) 2021 CESNET, z.s.p.o.
 #
 #  Redistribution and use in source and binary forms, with or without
 #  modification, are permitted provided that the following conditions
@@ -49,11 +50,20 @@ else()
         ${CMAKE_LIBRARY_PATH} ${CMAKE_INSTALL_PREFIX}/lib
     )
 
+    if(SYSREPO_INCLUDE_DIR)
+        find_path(SR_VERSION_PATH "sysrepo/version.h" HINTS ${SYSREPO_INCLUDE_DIR})
+        file(READ "${SR_VERSION_PATH}/sysrepo/version.h" SR_VERSION_FILE)
+        string(REGEX MATCH "#define SR_VERSION \"[0-9]+\\.[0-9]+\\.[0-9]+\"" SR_VERSION_MACRO "${SR_VERSION_FILE}")
+        string(REGEX MATCH "[0-9]+\\.[0-9]+\\.[0-9]+" SYSREPO_VERSION "${SR_VERSION_MACRO}")
+    endif()
+
     set(SYSREPO_INCLUDE_DIRS ${SYSREPO_INCLUDE_DIR})
     set(SYSREPO_LIBRARIES ${SYSREPO_LIBRARY})
     mark_as_advanced(SYSREPO_INCLUDE_DIRS SYSREPO_LIBRARIES)
 
     # handle the QUIETLY and REQUIRED arguments and set SYSREPO_FOUND to TRUE
     # if all listed variables are TRUE
-    find_package_handle_standard_args(Sysrepo DEFAULT_MSG SYSREPO_LIBRARY SYSREPO_INCLUDE_DIR)
+    find_package_handle_standard_args(Sysrepo FOUND_VAR SYSREPO_FOUND
+        REQUIRED_VARS SYSREPO_LIBRARY SYSREPO_INCLUDE_DIR
+        VERSION_VAR SYSREPO_VERSION)
 endif()
