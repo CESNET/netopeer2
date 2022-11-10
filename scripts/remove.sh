c806b6ac1567c6850e15eaeb8e2728cbdaecdcf1
Michal Vasko <mvasko@cesnet.cz>
2022-11-10 11:45:19 +0100

    scripts UPDATE handle unset USER env var
    Fixes #1295

--- scripts/remove.sh
+++ scripts/remove.sh
@@ -5,7 +5,7 @@
 if [ -n "$SYSREPOCTL_EXECUTABLE" ]; then
     SYSREPOCTL="$SYSREPOCTL_EXECUTABLE"
 # avoid problems with sudo PATH
-elif [ `id -u` -eq 0 ]; then
+elif [ `id -u` -eq 0 -a -n "$USER" ]; then
     SYSREPOCTL=`su -c 'command -v sysrepoctl' -l $USER`
 else
     SYSREPOCTL=`command -v sysrepoctl`
