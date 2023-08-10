846f516fa1eece435324dde141691fbe73a80fe3
Jeremie Leska <jeremie.leska@6wind.com>
2023-08-10 09:33:35 +0200

    tests UPDATE add configured subscriptions tests

--- tests/np_test.h
+++ tests/np_test.h
@@ -180,6 +180,11 @@
     "  <data/>\n" \
     "</get-config>\n"
 
+#define EMPTY_GET \
+    "<get xmlns=\"urn:ietf:params:xml:ns:netconf:base:1.0\">\n" \
+    "  <data/>\n" \
+    "</get>\n"
+
 #define ASSERT_EMPTY_CONFIG(state) \
     GET_CONFIG(state); \
     assert_string_equal(state->str, EMPTY_GETCONFIG); \
